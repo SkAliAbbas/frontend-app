9ced2f8c29317f3911bc3c6dd0715684c7d7d9fb
SkAliAbbas <shaikaliabbas10@gmail.com>
2026-03-13 10:03:51 +0530

    updated

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,16 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
+import { AppContext } from "../App";
 import "./Content.css";
 
 const API_URL = import.meta.env.VITE_API_URL;
 function Content() {
   // const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const { cart, setCart } = useContext(AppContext);
   const increment = () => {
     setCount(count + 1);
   };
   const decrement = () => {
     setCount(count - 1);
+  };
+  const addToCart = (product) => {
+    const existingItem = cart.find((item) => item._id === product._id);
+    if (existingItem) {
+      setCart(
+        cart.map((item) =>
+          item._id === product._id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
   };
   const fetchProducts = async () => {
     const url = `${API_URL}/store`;
@@ -34,7 +50,7 @@ function Content() {
             <h3>{product.name}</h3>
             <p>{product.desc}</p>
             <h4>{product.price}</h4>
-            <p><button>Add to Cart</button></p>
+            <p><button onClick={() => addToCart(product)}>Add to Cart</button></p>
           </div>
         ))}
       </div>
